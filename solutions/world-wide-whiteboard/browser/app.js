b916dfaaf3f54fc940bbd2cb9fedff9a38cf0003
Joseph Alves <databentpx@gmail.com>
2015-05-20 15:41:10 -0400

    Whiteboard solution

---
+++ solutions/world-wide-whiteboard/browser/app.js
@@ -0,0 +1,22 @@
+var socket = io(location.origin);
+
+socket.on('connect', function () {
+
+    socket.emit('join awesome room', location.pathname.slice(1) || 'default');
+
+    socket.on('board', function (draws) {
+        draws.forEach(function (draw) {
+            whiteboard.draw(draw.start, draw.end, draw.color);
+        });
+    });
+
+    whiteboard.on('draw', function (start, end, color) {
+        socket.emit('imDrawing', start, end, color);
+    });
+
+    socket.on('collaboratorDraw', function (start, end, color) {
+        whiteboard.draw(start, end, color);
+    });
+
+});
+
---
+++ solutions/world-wide-whiteboard/server.js
@@ -0,0 +1,48 @@
+var path = require('path');
+
+var http = require('http');
+var server = http.createServer();
+
+var express = require('express');
+var app = express();
+
+var socketio = require('socket.io');
+
+server.on('request', app);
+
+var io = socketio(server);
+
+var draws = {};
+
+io.on('connection', function (socket) {
+
+    console.log(socket.id, 'connected');
+
+    socket.on('join awesome room', function (roomName) {
+
+        socket.join(roomName);
+
+        if (!draws[roomName]) {
+            draws[roomName] = [];
+        } else {
+            socket.emit('board', draws[roomName]);
+        }
+
+        socket.on('imDrawing', function (start, end, color) {
+            draws[roomName].push({ start: start, end: end, color: color });
+            socket.broadcast.to(roomName).emit('collaboratorDraw', start, end, color);
+        });
+
+    });
+
+});
+
+server.listen(1337, function () {
+    console.log('The server is listening on port 1337!');
+});
+
+app.use(express.static(path.join(__dirname, 'browser')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
